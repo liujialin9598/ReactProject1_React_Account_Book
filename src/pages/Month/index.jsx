95b319218f556ef977e35e3d371a6eda82bf6a47
liuJalo <mrliujalo@foxmail.com>
2024-01-29 13:50:57 +0800

    统计区功能熟悉和静态结构搭建, Familiar with statistic area functions and static structure construction.;

--- src/pages/Month/index.jsx
+++ src/pages/Month/index.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { DatePicker, NavBar } from "antd-mobile";
+import React from "react";
+import './index.scss'
 
 export default function Month() {
   return (
-    <div>
-      month
+    <div className="monthlyBill">
+      <NavBar className="nav" backArrow={false}>
+        月度收支
+      </NavBar>
+
+      <div className="content">
+        <div className="header">
+          {/* 时间切换区域 date change area */}
+          <div className="date">
+            <span className="text">2023 | 3月账单</span>
+            <span className="arrow expand"></span>
+          </div>
+          {/* 统计区域 statistic area */}
+          <div className="twoLineOverview">
+            <div className="item">
+              <span className="money">{100}</span>
+              <span className="type">支出</span>
+            </div>
+
+            <div className="item">
+              <span className="money">{200}</span>
+              <span className="type">收入</span>
+            </div>
+
+            <div className="item">
+              <span className="money">{200}</span>
+              <span className="type">结余</span>
+            </div>
+
+            {/* 时间选择器 DatePicker */}
+            <DatePicker
+              className="kaDate"
+              title="记账 Account"
+              precision="month"
+              visible={false}
+              max={new Date()}
+            />
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
