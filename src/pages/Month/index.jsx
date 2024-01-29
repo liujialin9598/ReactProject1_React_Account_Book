3acd89bfb721c387e1dd4b026c750e7dcdd7f45a
liuJalo <mrliujalo@foxmail.com>
2024-01-29 10:43:56 +0800

    第一次用git更新

---
+++ src/pages/Month/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Month() {
+  return (
+    <div>
+      month
+    </div>
+  )
+}
---
+++ src/router/index.jsx
@@ -0,0 +1,26 @@
+import Month from "@/pages/Month";
+import News from "@/pages/News";
+import Year from "@/pages/Year";
+import Layout from "@/pages/layout";
+import { createBrowserRouter } from "react-router-dom";
+
+export default createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "month",
+        element: <Month />,
+      },
+      {
+        path: "year",
+        element: <Year />,
+      },
+    ],
+  },
+  {
+    path: "/new",
+    element: <News />,
+  },
+]);
